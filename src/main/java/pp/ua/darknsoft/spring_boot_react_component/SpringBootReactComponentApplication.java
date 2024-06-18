32e40a895b7e35cfb3e95dff2a185ba68b5427d4
InnaUsenko <innausenko8@gmail.com>
2024-06-18 15:36:26 +0300

    create teamleaf index.html

--- src/main/java/pp/ua/darknsoft/spring_boot_react_component/SpringBootReactComponentApplication.java
+++ src/main/java/pp/ua/darknsoft/spring_boot_react_component/SpringBootReactComponentApplication.java
@@ -2,12 +2,18 @@ package pp.ua.darknsoft.spring_boot_react_component;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootReactComponentApplication {
+public class SpringBootReactComponentApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactComponentApplication.class, args);
 	}
 
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
 }
