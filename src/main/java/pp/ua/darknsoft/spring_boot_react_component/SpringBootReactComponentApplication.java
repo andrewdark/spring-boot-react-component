e1c63058a6486e2cba60acd3e6fe02a8f4d22975
InnaUsenko <innausenko8@gmail.com>
2024-06-18 15:08:29 +0300

    create spring boot app

---
+++ src/main/java/pp/ua/darknsoft/spring_boot_react_component/SpringBootReactComponentApplication.java
@@ -0,0 +1,13 @@
+package pp.ua.darknsoft.spring_boot_react_component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pp/ua/darknsoft/spring_boot_react_component/SpringBootReactComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package pp.ua.darknsoft.spring_boot_react_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
